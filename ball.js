866f13152a31582bcb952e2bfca68e9f0c24ff15
Parmvir-S <parmvir.shergill@gmail.com>
2021-01-05 00:37:22 -0800

    added dropdown, printed out data, and added a photo

--- ball.js
+++ ball.js
@@ -1,20 +1,54 @@
-let endpoint = "https://www.balldontlie.io/api/v1/teams/28"
+/*let endpoint = "https://www.balldontlie.io/api/v1/teams/28"
 
 async function getTeam() {
     const response = await fetch(endpoint)
     const data = await response.json().catch(err => console.log(err, 'error'))
     //console.log(data.full_name)
     document.getElementById("displayteam").innerHTML = "Team Name: " + data.full_name + "<br>" + "Conference: " + data.conference + "<br>" + "Division: "  + data.division;
-}
-
+}*/
+setInterval(timefunc, 0);
+function timefunc() {
+    let d = new Date();
+    document.getElementById("timefunc").innerHTML=
+    d.getHours() + ":" +
+    d.getMinutes() + ":" +
+    d.getSeconds();
+  }
 
 function submittedit() {
     var ad = document.getElementById("team").value;
     console.log(ad);
-    
     var nbaTeamMapping = {
         'Atlanta':1,
-        'Boston':2                      
+        'Boston':2,
+        'Brooklyn':3,
+        'Charlotte':4,
+        'Chicago':5,
+        'Cleveland':6,
+        'Dallas':7,
+        'Denver':8,
+        'Detroit':9,
+        'Golden State':10,
+        'Houston':11,
+        'Indiana':12,
+        'LA Clippers': 13,
+        'LA Lakers':14,
+        'Memphis':15,
+        'Miami':16,
+        'Milwaukee':17,
+        'Minnesota':18,
+        'New Orleans':19,
+        'New York':20,
+        'Oklahoma':21,
+        'Orlando':22,
+        'Philadelphia':23,
+        'Phoenix':24,
+        'Portland':25,
+        'Sacramento':26,
+        'San Antonio':27,
+        'Toronto':28,
+        'Utah':29,
+        'Washington':30,
     };
     var nbaNumber = nbaTeamMapping[ad];
     var url = "https://www.balldontlie.io/api/v1/teams/" + nbaNumber;
@@ -24,8 +58,13 @@ function submittedit() {
 
 }
 
+
 function resolveHandler(respJson) {
-    
+    console.log(respJson)
+    document.getElementById("results").innerHTML = 
+    "Abbreviation: " + respJson.abbreviation + "<br>" + "City: " + respJson.city +
+    "<br>" + "Conference: " + respJson.conference + "<br>" + 
+    "Division: " + respJson.division + "<br>" + "Team Name: " + respJson.full_name; 
 }
 
 function rejectHandler(error) {
@@ -52,5 +91,3 @@ function getRequest(url) {
     });
     return promiseObj;
 }
-
-
