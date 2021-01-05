078998f5d2590464d5a390175816c9f911db6780
victorp24 <victor_guapo999@yahoo.com>
2021-01-04 22:42:26 -0800

    added basic template for get requests using promises and XHR

--- ball.js
+++ ball.js
@@ -9,8 +9,48 @@ async function getTeam() {
 
 
 function submittedit() {
-var ad = document.getElementById("team").value;
-endpoint = "https://www.balldontlie.io/api/v1/teams/28" + ad;
+    var ad = document.getElementById("team").value;
+    console.log(ad);
+    
+    var nbaTeamMapping = {
+        'Atlanta':1,
+        'Boston':2                      
+    };
+    var nbaNumber = nbaTeamMapping[ad];
+    var url = "https://www.balldontlie.io/api/v1/teams/" + nbaNumber;
+
+
+    getRequest(url).then(resolveHandler, rejectHandler);
+
+}
+
+function resolveHandler(respJson) {
+    
+}
+
+function rejectHandler(error) {
+    console.log(error);
+}
+
+function getRequest(url) {
+    var promiseObj = new Promise(function(resolve,reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function() {
+            if(xhr.status == 200) {
+                var resp = xhr.responseText;
+                var respJson = JSON.parse(resp);
+                resolve(respJson);
+            } else {
+                reject(new Error(xhr.responseText));
+            }
+        }
+        xhr.onerror = function() {
+            reject(new Error("Client-side error"));
+        }
+        xhr.send();
+    });
+    return promiseObj;
 }
 
 
